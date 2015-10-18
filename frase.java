18f43a51fcba32fa6926d5dfc116ab9d2ce87b82
ilopez1 <ivanlopez42@gmail.com>
2015-10-18 21:36:03 +0200

    comentari1

--- frase.java
+++ frase.java
@@ -6,7 +6,7 @@ public class MesPetit {
 	public static void main(String[] args) {
 		Scanner lector = new Scanner(System.in);
 
-		int [] array = {15,7,3,9,5,6};
+		int [] array = {15,7,3,9,5,6,7};
 		
 		for(int i=0; i < (array.length);i++){
 			for(int j=0; j < (array.length);j++){
