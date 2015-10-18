ac2ed8f50f23ed21e30c7db49b2387b4c70b97aa
ilopez1 <ivanlopez42@gmail.com>
2015-10-18 20:54:50 +0200

    commit

---
+++ frase.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class MesPetit {
+
+	public static void main(String[] args) {
+		Scanner lector = new Scanner(System.in);
+
+		int [] array = {15,7,3,9,5};
+		
+		for(int i=0; i < (array.length);i++){
+			for(int j=0; j < (array.length);j++){
+				if(array[i]<array[j]){
+					int variable=array[i];
+					array[i]=array[j];
+					array[j]=variable;
+				}
+				
+			}
+			
+		}
+		System.out.print(array[0]);
+	}
+}
